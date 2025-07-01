2d8d7bbdacd5cadb402d9b26483c6ca84c6d40f5
Abbasian.dev <mam-1371@outlook.com>
2025-07-01 22:54:17 +0330

    create ingredient chip widget

---
+++ lib/widget/ingredient_chip.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../model/ingredient.dart';
+import '../util/constants.dart';
+
+class IngredientChip extends StatelessWidget {
+  final Ingredient ingredient;
+  final VoidCallback? onDeleted;
+  final bool showDelete;
+
+  const IngredientChip({
+    super.key,
+    required this.ingredient,
+    this.onDeleted,
+    this.showDelete = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 8, bottom: 8),
+      child: Chip(
+        label: Text(
+          ingredient.toString(),
+          style: AppConstants.captionStyle.copyWith(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: AppConstants.primaryColor,
+        deleteIcon: showDelete
+            ? const Icon(
+          Icons.close,
+          size: 18,
+          color: Colors.white,
+        )
+            : null,
+        onDeleted: showDelete ? onDeleted : null,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppConstants.borderRadiusLarge),
+        ),
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+    );
+  }
+}
